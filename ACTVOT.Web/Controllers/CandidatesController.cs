a53bbaec0557eabf6877b7432a9a2eae915d0e9e
jcramirez <jcramirez@potobon.com.co>
2019-04-21 18:37:04 -0500

    Candidates funcionality in ActVotes

---
+++ ACTVOT.Web/Controllers/CandidatesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ACTVOT.Web.Data;
+using ACTVOT.Web.Data.Entities;
+
+namespace ACTVOT.Web.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CandidatesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Candidates
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Candidates.ToListAsync());
+        }
+
+        // GET: Candidates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidates = await _context.Candidates
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (candidates == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidates);
+        }
+
+        // GET: Candidates/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,name,LastName,Polparty")] Candidates candidates)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(candidates);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(candidates);
+        }
+
+        // GET: Candidates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidates = await _context.Candidates.FindAsync(id);
+            if (candidates == null)
+            {
+                return NotFound();
+            }
+            return View(candidates);
+        }
+
+        // POST: Candidates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,name,LastName,Polparty")] Candidates candidates)
+        {
+            if (id != candidates.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(candidates);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CandidatesExists(candidates.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(candidates);
+        }
+
+        // GET: Candidates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidates = await _context.Candidates
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (candidates == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidates);
+        }
+
+        // POST: Candidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var candidates = await _context.Candidates.FindAsync(id);
+            _context.Candidates.Remove(candidates);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CandidatesExists(int id)
+        {
+            return _context.Candidates.Any(e => e.Id == id);
+        }
+    }
+}
