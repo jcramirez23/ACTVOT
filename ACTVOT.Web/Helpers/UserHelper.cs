882e173eb1ff5eaf0d0d8681fd83d1631806f96a
jcramirez <jcramirez@potobon.com.co>
2019-04-21 15:37:16 -0500

    Change Pasword

--- ACTVOT.Web/Helpers/UserHelper.cs
+++ ACTVOT.Web/Helpers/UserHelper.cs
@@ -23,6 +23,12 @@
             return await this.userManager.CreateAsync(user, password);
         }
 
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword)
+        {
+            return await this.userManager.ChangePasswordAsync(user, oldPassword, newPassword);
+
+        }
+
         public async Task<User> GetUserByEmailAsync(string email)
         {        
             return await this.userManager.FindByEmailAsync(email);
@@ -43,6 +49,10 @@
             await this.signInManager.SignOutAsync();
         }
 
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await this.userManager.UpdateAsync(user);
+        }
     }
 
 }
